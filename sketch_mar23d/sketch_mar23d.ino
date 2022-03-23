4303763e690d7a2586deb1f6929d5914b0c3b832
juanborregoy <99011201+juanborregoy@users.noreply.github.com>
2022-03-23 12:28:56 +0100

    Add files via upload

---
+++ sketch_mar23d/sketch_mar23d.ino
@@ -0,0 +1,42 @@
+int dist;
+int strength;
+int chequeo;
+int i;
+int uart[9];
+const int HEADER=0x59;
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial1.begin(115200);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("  ");
+  if (Serial1.available()){
+    if (Serial1.read()==HEADER){
+      uart[0]=HEADER;
+      if (Serial1.read()==HEADER){
+        uart[1]=HEADER;
+        for (i=2;i<9;i++){
+          uart[i]=Serial.read();
+        }
+        chequeo=uart[0]+uart[1]+uart[2]+uart[3]+uart[4]+uart[5]+uart[6]+uart[7];
+        if (uart[8]==(chequeo&0xff)){
+          dist = uart [2]+uart[3]*256;
+          strength = uart [4]+uart[5]*256;
+          Serial.print("dist = ");
+          Serial.print(dist);
+          Serial.print('\t');
+          Serial.print("strength = ");
+          Serial.print(strength);
+          
+        }
+      }
+    }
+  }
+
+}
